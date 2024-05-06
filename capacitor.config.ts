f34e480c8051b34398112b03a284d5d3aed745ac
Ernandes Figueiredo <144298886+ernandes2023@users.noreply.github.com>
2024-05-06 00:34:25 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'nike',
+  webDir: 'www'
+};
+
+export default config;
